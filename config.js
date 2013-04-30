f599fafa23c62e81e303f1a9420bba776d1b39e2
kenoir <kenoir@gmail.com>
2013-04-30 12:57:44 +0100

    sending json response with CORS for jsfiddle

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    allowed_domains: [
+        'fiddle.jshell.net'
+        ],
+    salutation: { content: 'hello world' } 
+}
--- web.js
+++ web.js
@@ -1,12 +1,33 @@
 var express = require("express");
+var config = require("./config");
+
 var app = express();
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', config.allowed_domains);
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(allowCrossDomain);
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    var configured_response = config.salutation;
+
+    if(request.headers.accept == 'application/json'){ 
+        response.setHeader('Content-Type', 'applcation/json');
+        parsed_response = JSON.stringify(configured_response);
+    } else {
+        response.setHeader('Content-Type', 'text/plain');
+        parsed_response = configured_response.content;
+    }
+    response.send(parsed_response);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
